46d717060f23c6c49fc12843a76328b3fdb3c2c9
Themba Dombo <themba.dombo@gmail.com>
2021-07-06 21:41:57 +0200

    added parse url to begin access incoming requests

--- app/index.js
+++ app/index.js
@@ -5,11 +5,23 @@
 // Dependencies
 
 const http = require('http');
+const { URL } = require('url');
 
 // The server should respond to all request with a string
 
 const server = http.createServer(function(req, res){
+    // get url and parse it
+    console.log(req.url);
+    const baseURL = 'http://' + req.headers.host + '/';
+    const parseUrl = new URL(req.url,baseURL);
+
+    //then get path from url
+    const path = parseUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\?+$/g,'');
+    //then send response
     res.end("Hello World \n");
+    //log the request path
+    console.log('Request received on path '+trimmedPath);
 });
 // Start the server and have it liston port 3000
 server.listen(3000, function(){
